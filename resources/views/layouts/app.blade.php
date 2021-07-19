a4749119490b1f31b4fc8c7de6de0e3a887c8a4f
deu93 <deucalion93@ukr.net>
2021-07-19 17:21:14 +0300

    Updates 19.07.21

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <nav class="py-6 bg-white flex justify-between mb-6">
         <ul class="flex item-center">
             <li>
-                <a href="" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Posts</a>
@@ -27,7 +27,11 @@
                     <a href="" class="p-3">Alex</a>
                 </li>
                 <li>
-                    <a href="{{ route('logout') }}" class="p-3">Logout</a>
+                    <form action=" {{ route('logout') }}" method="post" class="p-3 inline">
+                        @csrf
+                        <button type="submit" >Logout</button>
+                    </form>
+                    
                 </li> 
             @endauth
 
