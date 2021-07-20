9f963fd24a93be0501cd6a8e73c47bf4b8b51357
deu93 <deucalion93@ukr.net>
2021-07-20 11:36:48 +0300

    Updates 20.07.21

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,14 +17,14 @@
                 <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
-                <a href="" class="p-3">Posts</a>
+                <a href="{{ route('posts') }}" class="p-3">Posts</a>
             </li>  
         </ul>
 
         <ul class="flex item-center">
             @auth
                 <li>
-                    <a href="" class="p-3">Alex</a>
+                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
                 </li>
                 <li>
                     <form action=" {{ route('logout') }}" method="post" class="p-3 inline">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -33,6 +35,9 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
+
+Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
+
